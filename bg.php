98307b8d53f28004067f3d384cd14504684cb787
Rene <renesegura@yahoo.com>
2020-10-07 11:33:18 -0700

    first commit

---
+++ bg.php
@@ -0,0 +1,6 @@
+<?php
+  $bg = array('bg-01.jpg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg' ); // array of filenames
+
+  $i = rand(0, count($bg)-1); // generate random number size of the array
+  $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+?>
